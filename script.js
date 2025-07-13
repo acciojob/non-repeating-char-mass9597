f1afb7decd7af37b511ad02d8dd8a0d4153f4b1c
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-13 07:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(int i=0; i<str.length; i++){
+		if(str.includes(str[i])){
+			continue;
+		}else{
+			console.log(str[i]);
+			return;
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
